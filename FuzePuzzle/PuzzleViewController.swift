a6f533b8250571dbc0409c35cfa055a1b7990f96
Franck Mamboue <mamboue@gmail.com>
2016-12-30 16:01:57 -0500

    Able to move tiles with tap

--- FuzePuzzle/PuzzleViewController.swift
+++ FuzePuzzle/PuzzleViewController.swift
@@ -19,6 +19,7 @@ class PuzzleViewController: UICollectionViewController {
   // MARK: Instance Variables
   var splitImages: [Tile] = []
   var missingTile = 0
+  var tileTapped: IndexPath?
   
   // MARK: Lifecycle
   override func viewDidLoad() {
@@ -121,12 +122,46 @@ class PuzzleViewController: UICollectionViewController {
   // MARK: Gesture Recognizers
   
   func prepareGestureRecognizers() {
-    let tapRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleGesture))
-    tapRecognizer.minimumPressDuration = 0.1
+    let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
     collectionView?.addGestureRecognizer(tapRecognizer)
   }
   
-  func handleGesture(gesture: UILongPressGestureRecognizer) {
+  func handleTapGesture(gesture: UITapGestureRecognizer) {
+    guard let collectionView = collectionView else {
+      return
+    }
+    
+    switch(gesture.state) {
+    case UIGestureRecognizerState.ended:
+      guard let selectedIndexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else {
+        break
+      }
+      tileTapped = selectedIndexPath
+      collectionView.beginInteractiveMovementForItem(at: selectedIndexPath)
+      
+      collectionView.endInteractiveMovement()
+      print("Tap")
+      if let neighbourMissingTileIndex = neighbourMissingTile() {
+        guard let tileTapped = tileTapped else {
+          return
+        }
+        self.missingTile = tileTapped.row
+        print("Missing Tile \(neighbourMissingTileIndex.row) is Neighbor")
+        collectionView.performBatchUpdates({
+          collectionView.moveItem(at: tileTapped, to: neighbourMissingTileIndex)
+          collectionView.moveItem(at: neighbourMissingTileIndex, to: tileTapped)
+        }, completion: {(finished) in
+          collectionView.dataSource?.collectionView!(collectionView, moveItemAt: tileTapped, to: neighbourMissingTileIndex)
+        })
+      } else {
+        print("I don't see missing tile!")
+      }
+    default:
+      collectionView.cancelInteractiveMovement()
+    }
+  }
+  
+  func handleLongPressGesture(gesture: UILongPressGestureRecognizer) {
     guard let collectionView = collectionView else {
       return
     }
@@ -136,14 +171,86 @@ class PuzzleViewController: UICollectionViewController {
       guard let selectedIndexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else {
         break
       }
+      tileTapped = selectedIndexPath
       collectionView.beginInteractiveMovementForItem(at: selectedIndexPath)
-    case UIGestureRecognizerState.changed:
-      collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: gesture.view!))
+    case UIGestureRecognizerState.changed: break
+    //      collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: gesture.view!))
     case UIGestureRecognizerState.ended:
       collectionView.endInteractiveMovement()
+      print("Tap")
+      if let neighbourMissingTileIndex = neighbourMissingTile() {
+        guard let tileTapped = tileTapped else {
+          return
+        }
+        self.missingTile = tileTapped.row
+        print("Missing Tile \(neighbourMissingTileIndex.row) is Neighbor")
+        collectionView.performBatchUpdates({
+          collectionView.moveItem(at: tileTapped, to: neighbourMissingTileIndex)
+          collectionView.moveItem(at: neighbourMissingTileIndex, to: tileTapped)
+        }, completion: {(finished) in
+          collectionView.dataSource?.collectionView!(collectionView, moveItemAt: tileTapped, to: neighbourMissingTileIndex)
+        })
+      } else {
+        print("I don't see missing tile!")
+      }
     default:
       collectionView.cancelInteractiveMovement()
     }
+  }
+}
+
+// MARK: UICollectionViewDataSource
+
+extension PuzzleViewController {
+  func neighbourMissingTile() -> IndexPath? {
+    guard let tileTapped = tileTapped else {
+      return nil
+    }
+    
+    var neighbours: [CGPoint] = []
+    let n = tileTapped.row
+    let x = n % Configs.numberOfTilesOnEdge
+    let y = n / Configs.numberOfTilesOnEdge
+    
+    let missingTileX = missingTile % Configs.numberOfTilesOnEdge
+    let missingTileY = missingTile / Configs.numberOfTilesOnEdge
+    
+    // Top
+    let topY = y - 1
+    if topY >= 0 && topY < Configs.numberOfTilesOnEdge {
+      let topNeighbour = CGPoint(x: x, y: topY)
+      neighbours.append(topNeighbour)
+    }
+    
+    // Bottom
+    let bottomY = y + 1
+    if bottomY >= 0 && bottomY < Configs.numberOfTilesOnEdge {
+      let bottomNeighbour = CGPoint(x: x, y: bottomY)
+      neighbours.append(bottomNeighbour)
+    }
+    
+    // Left
+    let leftX = x - 1
+    if leftX >= 0 && leftX < Configs.numberOfTilesOnEdge {
+      let leftNeighbour = CGPoint(x: leftX, y: y)
+      neighbours.append(leftNeighbour)
+    }
+    
+    // Right
+    let rightX = x + 1
+    if rightX >= 0 && rightX < Configs.numberOfTilesOnEdge {
+      let rightNeighbour = CGPoint(x: rightX, y: y)
+      neighbours.append(rightNeighbour)
+    }
+    
+    for neighbour in neighbours {
+      if Int(neighbour.x) == missingTileX && Int(neighbour.y) == missingTileY {
+        // We are using a flat array to represent the grid represented by the puzzle, so we use n = y * w + x to translate a grid item position to an array item position
+        return IndexPath(row: Int(neighbour.y) * Configs.numberOfTilesOnEdge + Int(neighbour.x), section: 0)
+      }
+    }
+    
+    return nil
   }
 }
 
@@ -165,13 +272,10 @@ extension PuzzleViewController {
     if let cell = cell as? TileCollectionViewCell {
       if indexPath.row == missingTile {
         cell.tileImageView.image = nil
-        cell.tileImageView.backgroundColor = UIColor.white
       } else {
         cell.tileImageView.image = splitImages[indexPath.row].image
       }
     }
-    
-    cell.backgroundColor = UIColor.black
     
     return cell
   }
---
+++ FuzePuzzle/SwappingCollectionView.swift
@@ -0,0 +1,9 @@
+//
+//  SwappingCollectionView.swift
+//  FuzePuzzle
+//
+//  Created by Zoumite Franck Armel Mamboue on 12/30/16.
+//  Copyright © 2016 Zoumite Franck Armel Mamboue. All rights reserved.
+//
+
+import Foundation
